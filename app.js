3e8073b76822d5e28f6927e45573286162fdf66f
Bruno Oliveira <brunowalker551@gmail.com>
2021-11-21 17:47:54 -0300

    script adjustments

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const countdown = () => {
     const countDate = new Date('Nov 30, 2021 00:00:00').getTime();
-    const now = new Date().getTime();
-    const gap = countDate - now;
+    const todayDate = new Date().getTime();
+    const gap = countDate - todayDate;
 
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
 
-    const textDay = Math.floor(gap / day);
-    const textHour = Math.floor((gap % day) / hour);
-    const textMinute = Math.floor((gap % hour) / minute);
-    const textSecond = Math.floor((gap % minute) / second);
+    const dayDisplay = Math.floor(gap / day);
+    const hourDisplay = Math.floor((gap % day) / hour);
+    const minuteDisplay = Math.floor((gap % hour) / minute);
+    const secondDisplay = Math.floor((gap % minute) / second);
 
-    document.querySelector('.day').innerText = textDay;
-    document.querySelector('.hour').innerText = textHour;
-    document.querySelector('.minute').innerText = textMinute;
-    document.querySelector('.second').innerText = textSecond;
+    document.querySelector('.day').innerText = dayDisplay;
+    document.querySelector('.hour').innerText = hourDisplay;
+    document.querySelector('.minute').innerText = minuteDisplay;
+    document.querySelector('.second').innerText = secondDisplay;
 }
 
 setInterval(countdown, 1000);
